f66c4497d363645d10da00dcdb54057574840b62
Vitor Alves <vitormanoel00710@gmail.com>
2022-03-17 17:47:27 -0300

    :rocket: Add mini-pojects dotnet

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Stopwatch {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            Start();
+            
+        }
+
+        static void Start(int time) {
+            int currentTime = 0;
+
+            while(currentTime != time) {
+                currentTime++;
+                Console.Clear();
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch has finished");
+            Thread.Sleep(2000);
+            
+        }
+
+        
+        
+    }
+}
