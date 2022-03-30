9ddac3075dfb6ab2730b503d82e30b7eb9970535
Vitor Alves <vitormanoel00710@gmail.com>
2022-03-29 23:57:56 -0300

    Add update exercice editorHtml

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -17,7 +17,7 @@ namespace EditorHtml {
 
         }
 
-        public static async void DrawScreen() {
+        public static void DrawScreen() {
 
             Console.Write("+");
             for(int i=0; i<30; i++) {
@@ -28,14 +28,44 @@ namespace EditorHtml {
 
             for(int lines = 0; lines < 10; lines++) {
                 System.Console.Write("|");
-                for(int i=0; i<=30; i++) {
+                for(int i=1; i<=30; i++) {
                     System.Console.Write(" ");
                 }
                 System.Console.Write("|");
                 Console.Write("\n");
             }
+
+            Console.Write("+");
+            for(int i=0; i<30; i++) {
+                Console.Write("-");
+            }
+            System.Console.Write("+");
+            Console.Write("\n");
         }
 
+
+        public static void WriteOptions() {
+
+            Console.SetCursorPosition(3,2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3,3);
+            Console.WriteLine("==============");
+            Console.SetCursorPosition(3,4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3,6);
+            Console.WriteLine("1 - Novo Arquivo");
+            Console.SetCursorPosition(3,7);
+            Console.WriteLine("2 - Abrir Arquivo");
+            Console.SetCursorPosition(3,8);
+            Console.WriteLine("0 - Sair");
+            
+            
+            
+            
+            
+            
+        } 
+        
 
     }
 }
